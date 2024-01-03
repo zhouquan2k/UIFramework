3aaf5a8db9e33c98ce902eac69667c2c1b53e4e4
zhouquan <zhouquan2k@gmail.com>
2024-01-02 19:50:54 -0500

    commit

--- gen_environment_variables.sh
+++ gen_environment_variables.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ROOT_DIR=/usr/share/nginx/html
 # Replace env vars in files served by NGINX
-for file in $ROOT_DIR/static/js/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
+for file in $ROOT_DIR/js/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
 do
   sed -i 's|VUE_APP_ENV_PLACEHOLDER|'${VUE_APP_ENV}'|g' $file
   # Your other variables here...
