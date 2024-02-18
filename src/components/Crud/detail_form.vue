64a3c8542340cba5ff00162c885a1d83cdcb45e4
zhouquan2k <zhouquan2k@gmail.com>
2024-02-18 11:58:57 -0500

    - Fix： Date issue， Timestamp display

--- src/components/Crud/detail_form.vue
+++ src/components/Crud/detail_form.vue
@@ -3,10 +3,12 @@
         <el-row>
             <el-col :key="`col-${field.name}`" v-for="field in metadata.fields"
                 :span="24 / (field.type == 'Text' ? 1 : formCols)"
-                v-if="!field.hidden && (['IDStr', 'Integer', 'String', 'Enum', 'Dictionary', 'Text', 'Decimal', 'ToMany', 'ToOne', 'Date'].includes(field.type) || field.uiType)">
+                v-if="!field.hidden && (['IDStr', 'Integer', 'String', 'Enum', 'Dictionary', 'Text', 'Timestamp', 'Decimal', 'ToMany', 'ToOne', 'Date'].includes(field.type) || field.uiType)">
                 <el-form-item :name="field.name" :label="field.label" :prop="field.name">
-                    <span v-if="mode == 'readonly' || (mode == 'update' && !field.updatable)">{{ detail[field.name]
-                    }}</span>
+                    <span
+                        v-if="mode == 'readonly' || (mode == 'update' && !field.updatable) || field.type == 'Timestamp'">{{
+                            detail[field.name]
+                        }}</span>
                     <component v-else-if="field.uiType" :is="getCustomComponent()[field.uiType]"
                         v-bind="{ field: field, row: detail, params: getCustomComponent()[field.uiType + '_params'] }"
                         v-model="detail[field.name]" />
@@ -17,7 +19,7 @@
                     <DictionarySelect v-model="detail[field.name]" v-else-if="['Enum', 'Dictionary'].includes(field.type)"
                         :dictionary="field.typeName" />
                     <el-date-picker v-else-if="['Date'].includes(field.type)" v-model="detail[field.name]" type="date"
-                        placeholder="选择日期">
+                        placeholder="选择日期" :value-format="globalDateFormat">
                     </el-date-picker>
                     <el-select v-else-if="['ToMany'].includes(field.type) && toManySelectData[field.name]"
                         v-model="detail[field.name]" multiple :placeholder="`请选择${field.label}`"
@@ -27,7 +29,7 @@
                         </el-option>
                     </el-select>
                     <DetailForm v-else-if="['ToOne'].includes(field.type)" :name="field.typeName"
-                        :detail="detail[field.name]" :mode="mode" />
+                        :detail="detail[field.name]" :mode="mode" :value-format="globalDateFormat" />
                 </el-form-item>
             </el-col>
             <slot name="fields" />
@@ -35,7 +37,7 @@
     </el-form>
 </template>
 <script>
-import { initMetadata } from '@/utils/utils';
+import { initMetadata, globalDateFormat } from '@/utils/utils';
 import DictionarySelect from '@/components/dictionary_select.vue';
 export default {
     name: 'DetailForm',
@@ -52,6 +54,7 @@ export default {
     components: { DictionarySelect },
     data() {
         return {
+            globalDateFormat,
             rules: {},
             metadata: null,
         };
